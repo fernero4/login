88aff4ab7e097d6ca219e3f15f174e0e470f682f
fernando horta <fernanhorta@hotmail.com.ar>
2023-02-22 13:08:25 -0300

    Update basicLoginForm.py

--- basicLoginForm.py
+++ basicLoginForm.py
@@ -3,16 +3,25 @@ from tkinter import messagebox
 
 IMAG_PATH="C:/Users/ferna/Downloads/python/login/Images/"
 
-# def Login():
-#     username=entry1.get()
-#     password=entry2.get()
+def Singin():
+    username=user.get()
+    password=code.get()
 
-#     if (username=="" and password==""):
-#         messagebox.showinfo("","Blank not allowed")
-#     elif (username=="ferna" and password=="1234"):
-#         messagebox.showinfo("","login success")
-#     else:
-#         messagebox.showinfo("","Incorrect username and password")
+    if (username=="admin" and password=="1234"):
+        screen=Toplevel(root)
+        screen.title("Example App")
+        screen.geometry("925x500+300+200")
+        screen.config(bg="white")
+        Label(screen, text="Sample message", bg="#fff", font=("Calibri(Body)", 50, "bold")).pack(expand=True)
+        screen.mainloop()
+
+    elif (username!="admin" and password!="1234"):
+        messagebox.showerror("Invalid","Invalid username and password")
+    elif (username!="admin"):
+        messagebox.showerror("Invalid","Invalid username")
+    elif (password!="1234"):
+        messagebox.showerror("Invalid","Invalid password")
+
 
 root=Tk()
 root.title("Login Form")
@@ -31,24 +40,45 @@ heading.place(x=100, y=5)
 
 #User
 
+def on_enter(e):
+    user.delete(0, 'end')
+
+def on_leave(e):
+    name=user.get()
+    if name=="":
+        user.insert(0, "Username")
+
+
 user=Entry(frame, fg="black", border=0, bg="white", font=("Microsoft YaHei UI Light", 11))
 user.place(x=30, y=80)
 user.insert(0, "Username")
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
 
 Frame(frame, width=295, height=2, bg="black").place(x=25, y=107)
 
 #Code
+def on_enter(e):
+    code.delete(0, 'end')
+
+def on_leave(e):
+    name=user.get()
+    if name=="":
+        user.insert(0, "Password")
+
 code=Entry(frame, width=25, fg="black", border=0, bg="white", font=("Microsoft YaHei UI Light", 11))
 code.place(x=30, y=150)
 code.insert(0, "Password")
+code.bind('<FocusIn>', on_enter)
+code.bind('<FocusOut>', on_leave)
+
 
 Frame(frame, width=295, height=2, bg="black").place(x=25, y=177)
 
 
-
-
-
-
+Button(frame, width=39, padx=7, text="Sing in", bg="#57a1f8", fg="white", border=0, command=Singin).place(x=35, y=204)
+label=Label(frame, text="Don't hace an account?", fg="black", bg="white", font=("Microsoft YaHei UI Light", 9)).place(x=75, y=270)
+sing_up=Button(frame, width=6, text="Sing up", border=0, bg="white", cursor="hand2", fg="#57a1f8").place(x=215, y=270)
 
 
 
